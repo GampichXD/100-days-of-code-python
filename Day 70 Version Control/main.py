0bb680fc17e30046c27261a87db4d6aa692b9dc5
GampichXD <abrahambram295@gmail.com>
2025-05-11 22:31:27 +0700

    day 70

--- Day 70 Version Control/main.py
+++ Day 70 Version Control/main.py
@@ -9,3 +9,20 @@
 # .gitignore
 # template : https://github.com/github/gitignore
 # git rm --cached {<file> / -r .} --> remove file from staging area
+# git clone <url> --> clone remote repository ==> build on existing project, improve your own version, read other people's code, contribute to open source projects
+# Branching and Merging
+# git branch <branch_name> --> create a new branch
+# Main , Feature, Bugfix, Release, Hotfix
+
+
+# Additional Challenges
+# Cherry picking
+# git cherry-pick <commit_hash> --> apply changes from a specific commit to the current branch
+# Git Rebase
+# git rebase <branch_name> --> apply changes from one branch to another
+
+# Forking and Pull Requests
+# Owner and Collaborator can write access
+# Contributor and Everyone else just read-only access
+# Pull Request --> request to merge changes from one branch to another
+# git pull request <branch_name> --> create a pull request
